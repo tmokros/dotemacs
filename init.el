45860a37badfc2062f7b17dc5d053f09fc0ba361
Todd Mokros <tmokros@tmokros.net>
2016-02-17 19:32:28 -0500

    Disable melpa

--- init.el
+++ init.el
@@ -31,8 +31,8 @@
 (server-start)
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;(add-to-list 'package-archives
+;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
